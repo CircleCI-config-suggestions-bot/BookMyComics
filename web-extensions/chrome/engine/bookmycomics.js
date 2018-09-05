ab45bb696f3ee07e73cc27bce9253852b6591d3d
David Pineau <dav.pineau@gmail.com>
2018-09-05 16:02:09 +0200

    [Engine] Fix typos in BmcEngine.track()

--- web-extensions/chrome/engine/bookmycomics.js
+++ web-extensions/chrome/engine/bookmycomics.js
@@ -9,14 +9,14 @@ function BmcEngine() {
 }
 
 BmcEngine.prototype.track = function(comicName, chapter, page) {
-    console.log(`BookMyComic: bmcEngine.track: manga=${manga} chapter=${chapter} page=${page}`);
-    this._db.findComic(manga, (err, id) => {
+    console.log(`BookMyComic: bmcEngine.track: manga=${comicName} chapter=${chapter} page=${page}`);
+    this._db.findComic(comicName, (err, comicId) => {
         if (err) {
             console.log('BookMyComic: Could not track comic: Database error (step1)');
             return ;
         }
-        console.log(`BookMyComic: bmcEngine.track: Got id from storage: ${id}`);
-        if (id === null) {
+        console.log(`BookMyComic: bmcEngine.track: Got comicId from storage: ${comicId}`);
+        if (comicId === null) {
             return this._ui.makeRegisterDialog();
         }
         this._db.updateComic(comicId, chapter, page,
