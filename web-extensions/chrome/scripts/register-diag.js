14f9a4362534ee4747dba915a3feda60fb9bb881
David Pineau <dav.pineau@gmail.com>
2018-10-05 01:05:09 +0200

    [InfoBar] Transform infobar into a single '+' button
    As suggested during a UI design session, having a single UI (the
    sidebar) to handle most operations, including "register" and "alias"
    seems like a good UX improvement.
    
    As such, this committransforms the existing buttons into a single one,
    that we'll use to interact later on with the SideBar.

--- web-extensions/chrome/scripts/register-diag.js
+++ web-extensions/chrome/scripts/register-diag.js
@@ -4,59 +4,14 @@ const comicName = decodeURI(uriParams[0].split('=')[1]);
 const chapter = uriParams[1].split('=')[1];
 const page = uriParams[2].split('=')[1];
 console.log(`BmcInfoBar: comic ${comicName}, chapter=${chapter}, page=${page}`);
+document.getElementById('register').addEventListener('click', () => {
+    console.log('Clicked "AddComic"');
 
-
-document.getElementById('registerComic').addEventListener('click', () => {
-    console.log('Clicked "Register"');
-    const engine = new BmcEngine();
-    engine.addEventListener(engine.events.register.error, err => {
-    });
-    engine.addEventListener(engine.events.register.complete, () => {
-        console.log('Messaging main window for InfoBar removal');
-        const dataObj = {
-            type: 'action',
-            action: 'RemoveInfoBar',
-        };
-        return window.parent.postMessage(JSON.stringify(dataObj), '*');
-    });
-    return engine.register(comicName, chapter, page);
+   const dataObj = {
+       type: 'action',
+       action: 'AddComic',
+   };
+   return window.parent.postMessage(JSON.stringify(dataObj), '*');
 });
 
-document.getElementById('aliasComic').addEventListener('click', () => {
-    // XXX TODO FIXME
-    // Ideally, this function should spawn a drop-down overlay menu to offer
-    // the possibility to select an existing tracked manga to link the current
-    // page to.
-    // XXX TODO FIXME
-    console.log('Clicked "Alias"');
-    const engine = new BmcEngine();
-    engine.addEventListener(engine.events.alias.error, err => {
-    });
-    engine.addEventListener(engine.events.alias.complete, () => {
-        console.log('Messaging main window for InfoBar removal');
-        const dataObj = {
-            type: 'action',
-            action: 'RemoveInfoBar',
-        };
-        return window.parent.postMessage(JSON.stringify(dataObj), '*');
-    });
-    engine.alias(comicName, chapter, page);
-});
-
-document.getElementById('ignoreComic').addEventListener('click', () => {
-    console.log('Clicked "Ignore"');
-    const engine = new BmcEngine();
-    engine.addEventListener(engine.events.ignore.error, err => {
-    });
-    engine.addEventListener(engine.events.ignore.complete, () => {
-        console.log('Messaging main window for InfoBar removal');
-        const dataObj = {
-            type: 'action',
-            action: 'RemoveInfoBar',
-        };
-        return window.parent.postMessage(JSON.stringify(dataObj), '*');
-    });
-    engine.ignore(comicName, chapter, page);
-});
-
-console.log('Registered click events for register, alias and ignore buttons');
+console.log('Register InfoBar setup done.');
