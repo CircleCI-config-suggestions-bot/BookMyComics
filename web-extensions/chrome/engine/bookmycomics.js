CircleCI-config-suggestions-bot/BookMyComics

commit 611780d1639f5c4cf09d979f05c375347af655c9

[Refactor] Decouple BmcEngine from Sidebar
In order to better identify the role of each module within the
web-extension, it was necessary to ensure that only one instance of each
logic-critical unit was existing at any time on the whole page.

To do so, the sidebar scripts are now using messages to request Data
operations to the Core (BmcEngine), and using utility classes
(BmcDataAPI and BmcMessagingHandler) to handle minor tasks.

As such, this commit:
 - Adds a message handling in BmcEngine, to act as messaging proxy for
   methods such as register & alias
 - Use the BmcEngine mesages from the sidebar to instruct the core to
   register/alias a comic
 - Adds a message handling in the Sidebar to allow toggling it from any
   other frame
 - Use the toggle message from the UI module to show/hide the sidebar

web-extensions/chrome/engine/bookmycomics.js
@@ -1,3 +1,5 @@
+const ENGINE_ID = 'BookMyComics::Engine';
+
 /**
  * This class provides the internal logic for the extension.
  * Depending on the method, it can optionally spawn an UI element, or generate
@@ -19,6 +21,36 @@ function BmcEngine(hostOrigin, readerName, comicName, chapter, page) {
     this.addEventListener = this._eventCore.addEventListener.bind(this._eventCore);
     this.removeEventListener = this._eventCore.removeEventListener.bind(this._eventCore);
     this.dispatchEvent = this._eventCore.dispatchEvent.bind(this._eventCore);
+
+    // Setup listeners for messages that should be directly handled by the core
+    // Handle "Register"
+    this._messaging.addWindowHandler(
+        ENGINE_ID,
+        evData => evData.type === 'action' && evData.action === 'register',
+        evData => {
+            this.register(evData.label, err => {
+                let retErr = null;
+                if (err) {
+                    retErr = new Error('Could not register Comic ' + evData.label + ': ' + err.message);
+                }
+                this._ui.toggleSidePanel();
+                notifyResult('Register Comic', retErr);
+            });
+        });
+    // Handle "Alias"
+    this._messaging.addWindowHandler(
+        ENGINE_ID,
+        evData => evData.type === 'action' && evData.action === 'alias',
+        evData => {
+            this.alias(evData.id, err => {
+                let retErr = null;
+                if (err) {
+                    retErr = new Error('Could not alias current page to comicId ' + evData.id + ': ' + err.message);
+                }
+                this._ui.toggleSidePanel();
+                notifyResult('Alias Comic', retErr);
+            });
+        });
 
     // A bit of stateful data, so that we can avoid re-checking the storage
     // everytime (and thus speed-up a bit the logic that spawn the UI bits)
@@ -44,18 +76,6 @@ function BmcEngine(hostOrigin, readerName, comicName, chapter, page) {
  */
 BmcEngine.prototype.events = {
     load: 'engine.load',
-    register: {
-        error: 'register.error',
-        complete: 'register.done',
-    },
-    alias: {
-        error: 'alias.error',
-        complete: 'alias.done',
-    },
-    ignore: {
-        error: 'ignore.error',
-        complete: 'ignore.done',
-    },
 };
 
 /**
@@ -175,15 +195,14 @@ BmcEngine.prototype.track = function() {
  *
  * @return {undefined}
  */
-BmcEngine.prototype.register = function(label) {
+BmcEngine.prototype.register = function(label, cb) {
     console.log(`BookMyComic: bmcEngine.register: label=${label} reader=${this._comic.reader} manga=${this._comic.name} chapter=${this._comic.chapter} page=${this._comic.page}`);
     return this._db.registerComic(label, this._comic.reader, this._comic.name, this._comic.chapter, this._comic.page, err => {
         console.warn(`Register completed with ERR=${err}`);
-        if (err) {
-            return this.dispatchEvent(new CustomEvent(this.events.register.error, {detail: err}));
+        if (!err) {
+            this._forceMemoizeComic();
         }
-        this._forceMemoizeComic();
-        return this.dispatchEvent(new CustomEvent(this.events.register.complete));
+        return cb(err);
     });
 };
 
@@ -195,24 +214,12 @@ BmcEngine.prototype.register = function(label) {
  *
  * @return {undefined}
  */
-BmcEngine.prototype.alias = function(comicId) {
+BmcEngine.prototype.alias = function(comicId, cb) {
     console.log(`BookMyComic: bmcEngine.alias: id=${comicId} reader=${this._comic.reader} manga=${this._comic.name}`);
     return this._db.aliasComic(comicId, this._comic.reader, this._comic.name, err => {
-        if (err) {
-            return this.dispatchEvent(new CustomEvent(this.events.alias.error, {detail: err}));
+        if (!err) {
+            this._forceMemoizeComic();
         }
-        this._forceMemoizeComic();
-        return this.dispatchEvent(new CustomEvent(this.events.alias.complete));
+        return cb(err);
     });
-};
-
-/*
- * This function registers a comic into the "Ignore List" in the saved data.
- * TODO: Ideally, it should be registered along with some data allowing the
- * extension to offer registering the manga again, according to the user
- * settings.
- */
-BmcEngine.prototype.ignore = function() {
-    console.log(`BookMyComic: bmcEngine.ignore: manga=${this._comic.name} chapter=${this._comic.chapter} page=${this._comic.page}`);
-    this.dispatchEvent(new CustomEvent(this.events.ignore.complete));
 };

web-extensions/chrome/engine/ui.js
@@ -86,6 +86,19 @@ BmcUI.prototype.buildSidePanel = function(setupTracker, resourcePath) {
     });
 };
 
+BmcUI.prototype.toggleSidePanel = function() {
+    const evData = {
+        type: 'action',
+        action: 'toggle',
+        module: 'sidebar',
+    };
+    const sidepanel = FrameFinder.findWindow(FrameFinder.definitions.SIDEPANEL);
+    if (!sidepanel) {
+        return ;
+    }
+    sidepanel.postMessage(evData, '*');
+}
+
 BmcUI.prototype.makeRegisterDialog = function(comicName, chapter, page) {
     var bro = getBrowser();
     this.makeInfobar(bro.runtime.getURL('register-diag.html'));

web-extensions/chrome/scripts/load-bookmarks.js
@@ -6,8 +6,9 @@ const chapter = uriParams[3].split('=')[1];
 const page = uriParams[4].split('=')[1];
 console.log(`BmcSideBar: origin ${hostOrigin} : comic ${comicName}, chapter=${chapter}, page=${page}`);
 
-console.log('Loading Engine');
-const bmcEngine = new BmcEngine(hostOrigin, readerName, comicName, chapter, page);
+console.log('Loading required Bmc utilities');
+const bmcMessaging = new BmcMessagingHandler(hostOrigin);
+const bmcDb = new BmcDataAPI();
 
 function BmcMangaList() {
     this._node = document.getElementById('manga-list');
@@ -19,26 +20,13 @@ BmcMangaList.prototype.MODE_BROWSE = 'browse';
 
 BmcMangaList.prototype.onAliasClick = function(ev) {
     const comicLabel = ev.target;
-    console.log(`BmcSideBar: BmcMangaList: onAlias: Label=${ev.target.innerText} id=${ev.target.id} reader=${readerName} name=${comicName}`);
-    const handleAliasError = err => {
-        bmcEngine.removeEventListener(bmcEngine.events.alias.error, handleAliasError);
-        bmcEngine.removeEventListener(bmcEngine.events.alias.complete, handleAliasSuccess);
-        this.setMode(this.MODE_BROWSE);
-        showHideSidePanel();
-        notifyResult('Alias Comic', new Error('Could not alias current page to comicId ' + comicId + ': ' + err.message));
+    console.log(`BmcSideBar: BmcMangaList: onAlias: Label=${ev.target.innerText} id=${ev.target.id}`);
+    const evData = {
+        type: "action",
+        action: "alias",
+        id: comicLabel.bmcData.id,
     };
-    const handleAliasSuccess = () => {
-        bmcEngine.removeEventListener(bmcEngine.events.alias.error, handleAliasError);
-        bmcEngine.removeEventListener(bmcEngine.events.alias.complete, handleAliasSuccess);
-        this.setMode(this.MODE_BROWSE);
-        showHideSidePanel();
-        notifyResult('Alias Comic', null);
-    };
-    bmcEngine.addEventListener(bmcEngine.events.alias.error, handleAliasError);
-    bmcEngine.addEventListener(bmcEngine.events.alias.complete, handleAliasSuccess);
-    // Don't need to provide the reader/name/chapter/pages, as they're
-    // already set in the engine since the instanciation.
-    bmcEngine.alias(comicLabel.bmcData.id);
+    window.top.postMessage(evData, '*');
 }
 
 BmcMangaList.prototype.onBrowseClick = function(ev) {
@@ -109,7 +97,7 @@ BmcMangaList.prototype.generate = function() {
     }
 
     // Now that the parent is a clean slate, let's generate
-    bmcEngine._db.list((err, comics) => {
+    bmcDb.list((err, comics) => {
        comics.forEach(comic =>
             mangaList.appendChild(this.generateComic(comic))
        );
@@ -215,34 +203,34 @@ function addEvents(mangaList) {
             }
 
             // Now do the actual registration
-            const handleRegisterError = err => {
-                bmcEngine.removeEventListener(bmcEngine.events.register.error, handleRegisterError);
-                bmcEngine.removeEventListener(bmcEngine.events.register.complete, handleRegisterSuccess);
-                mangaList.setMode(mangaList.MODE_BROWSE);
-                showHideSidePanel();
-                notifyResult('Register Comic', new Error('Could not alias current page to comicId ' + comicId));
+            const evData = {
+                type: "action",
+                action: "register",
+                label,
             };
-            const handleRegisterSuccess = () => {
-                bmcEngine.removeEventListener(bmcEngine.events.register.error, handleRegisterError);
-                bmcEngine.removeEventListener(bmcEngine.events.register.complete, handleRegisterSuccess);
-                mangaList.setMode(mangaList.MODE_BROWSE);
-                showHideSidePanel();
-                notifyResult('Register Comic');
-            };
-            bmcEngine.addEventListener(bmcEngine.events.register.error, handleRegisterError);
-            bmcEngine.addEventListener(bmcEngine.events.register.complete, handleRegisterSuccess);
-            // Don't need to provide the reader/name/chapter/pages, as they're
-            // already set in the engine since the instanciation.
-            bmcEngine.register(label);
+            window.top.postMessage(evData, '*');
         };
     }
 
-    bmcEngine._messaging.addWindowHandler(
+    bmcMessaging.addWindowHandler(
         BmcUI.prototype.SIDEPANEL_ID,
         evData => evData.type === 'action' && evData.action === 'notification',
         evData => {
             console.log(`BmcSidePanel: received message to display status notification op=${evData.operation} err=${evData.error}`);
             notifyResult(evData.operation, evData.error);
+        });
+    bmcMessaging.addWindowHandler(
+        BmcUI.prototype.SIDEPANEL_ID,
+        evData => evData.type === 'action' && evData.action === 'setup' && evData.operation === 'register',
+        evData => {
+            console.log('BmcSidePanel: Handling request to show Register button');
+            showRegisterButton();
+        });
+    bmcMessaging.addWindowHandler(
+        BmcUI.prototype.SIDEPANEL_ID,
+        evData => evData.type === 'action' && evData.action === 'toggle' && evData.module === 'sidebar',
+        evData => {
+            showHideSidePanel();
         });
 }
 

commit 0a4dc9204ac464f494df854d5a9ad732b8a9ee9f

[Engine] Fix notification system (bug introduced by 611780d1)
Commit 611780d1 mistakenly tried to use a function that was not available in
the context, since it was left in the original file.

This commit fixes the situation by forwarding the proper values through the
messaging system, making use of the existing code handling the notification in
the Sidebar's javascript code.

web-extensions/chrome/engine/bookmycomics.js
@@ -36,7 +36,7 @@ function BmcEngine(hostOrigin, readerName, comicInfo) {
                                                                 'err': err.message}));
                 }
                 this._ui.toggleSidePanel();
-                notifyResult('Register Comic', retErr);
+                this._ui.makeNotification('Register Comic', retErr);
             });
         });
     // Handle "Alias"
@@ -51,7 +51,7 @@ function BmcEngine(hostOrigin, readerName, comicInfo) {
                                                                 'err': err.message}));
                 }
                 this._ui.toggleSidePanel();
-                notifyResult('Alias Comic', retErr);
+                this._ui.makeNotification('Alias Comic', retErr);
             });
         });
     // Handle "Delete"
@@ -73,7 +73,7 @@ function BmcEngine(hostOrigin, readerName, comicInfo) {
                     }
                     this._ui.refreshSidePanel();
                     const kind = evData.source ? "Comic Source" : "Comic"
-                    notifyResult(`Delete ${kind}`, retErr);
+                    this._ui.makeNotification(`Delete ${kind}`, retErr);
                 });
         });
     // Handle "URLOpen"
@@ -217,7 +217,7 @@ BmcEngine.prototype.track = function() {
             return;
         }
         this._db.updateComic(this._comic.id, this._comic.chapter, this._comic.page,
-                             err => this._ui.makeTrackingNotification(err));
+                             err => this._ui.makeNotification('Track Comic', err));
     }, {once: true});
 
     // Now fire the load or cache hit, that shall trigger the previously

web-extensions/chrome/engine/ui.js
@@ -112,12 +112,12 @@ BmcUI.prototype.removeSidePanel = function() {
     this._messaging.removeWindowHandlers(this.SIDEPANEL_ID);
 };
 
-BmcUI.prototype.makeTrackingNotification = function(err) {
+BmcUI.prototype.makeNotification = function(operation, err) {
     var evData = {
         type: "action",
         action: "notification",
-        operation: "track",
-        error: err,
+        operation,
+        error: (err||{}).message,
     };
     const sidepanel = FrameFinder.findWindow(FrameFinder.definitions.SIDEPANEL);
     if (!sidepanel) {

web-extensions/chrome/scripts/load-bookmarks.js
@@ -407,7 +407,7 @@ function notifyResult(operation, error) {
     const togBtn = document.getElementById('hide-but');
     triggerTransition(togBtn, transitionColor);
     if (error) {
-        LOGS.error('E0016', {'operation': operation, 'error': error.message});
+        LOGS.error('E0016', {operation, 'error': error.message});
     }
 }
 