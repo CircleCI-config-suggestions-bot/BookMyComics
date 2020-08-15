45a867c49fb5162757754a8ae4c1c106aa7081a5
David Pineau <dav.pineau@gmail.com>
2020-08-15 16:10:37 +0200

    [Tests/Readers][mangahere] Make function more reliable
    This is done through various additions and updates:
     - Ensure that the link of the randomly selected chapter redirect to the same
       website, as mangahere sometimes redirects to `manganelos.com`
     - Use javascript execution to click on one of the chapter links in a fail-safe
       way, along with a proper wait method to ensure the new page is loaded
     - Add Selenium's TimeoutException in the retriable Exceptions list
    
    Fixes #73

--- tests/func/utils/__init__.py
+++ tests/func/utils/__init__.py
@@ -3,6 +3,9 @@ from functools import reduce
 import sys
 import time
 
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+
 def make_realpath(pathlist):
     fpath = reduce(lambda p, p2: path.join(p, p2), pathlist, '')
     return path.realpath(fpath)
@@ -25,9 +28,18 @@ def retry(retries=5, abort=True):
                 except RetriableError as e:
                     count += 1
                     print(str(e), file=sys.stderr)
+                except TimeoutException as e:
+                    count += 1
+                    print(str(e), file=sys.stderr)
 
             # Assert if abort is set, when all retries are exhausted
             assert abort == False
 
         return wrapper
     return decorator
+
+
+def wait_for_next_page(driver, prev_url):
+    wait = WebDriverWait(driver, 10)
+    wait.until(lambda driver: (driver.current_url != prev_url
+                               and driver.execute_script('return document.readyState') == 'complete'))
