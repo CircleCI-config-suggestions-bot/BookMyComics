621fd1fee81a8d3561c422606287fdab58dd4e50
David Pineau <dav.pineau@gmail.com>
2021-07-18 20:43:45 +0200

    #42 [Support/IsekaiScan] Stabilize Load test & Update support
    This commit provides multiple changes to the IsekaiScan support, improving test
    reliability, and the reader's feature coverage:
    
    Test Utilities:
     - Add RGPD consent auto-acceptation in test utilities (Popups block actions otherwise)
     - Ignore chapter URLs including two numbers in the URL (ex: `chapter-0-2`
       which denotes a secondary version of chapter 0), to stick with unified
       compatibility of our engine.
     - Add missing test utility method
     - Add support for both `ch-` and `chapter-` prefixes in URL for chapter numbering
    
    Support module:
     - Add support for both `ch-` and `chapter-` prefixes in URL for chapter numbering
     - Update code relatively to HTML/CSS structure changes on the reader

--- tests/func/utils/support/isekaiscan.py
+++ tests/func/utils/support/isekaiscan.py
@@ -13,11 +13,22 @@ class IsekaiScanDriver(SupportBase):
     def __init__(self, *args, **kwargs):
         super(IsekaiScanDriver, self).__init__(*args, **kwargs)
 
-    def _get_mangas(self):
+    def home(self):
         # First, go to the website
         self._driver.get('https://isekaiscan.com/')
+        # pass RPGD cookie consent button if any, since only necessary on the
+        # first load during a testing session.
+        rgpd_consent_btn = self._driver.find_elements(by=By.CSS_SELECTOR, value='.fc-cta-consent')
+        if rgpd_consent_btn:
+            rgpd_consent_btn[0].click()
+
+    def _get_mangas(self):
+        self.home()
         # Retrieve the generated random link, which is generated after loading the page
         return self._driver.find_elements(by=By.CSS_SELECTOR, value='.chapter-item')
+
+    def _get_sep(self, url):
+        return 'ch-' if '/ch-' in url else 'chapter-'
 
     @retry(abort=True)
     def load_random(self, predicate=None):
@@ -30,7 +41,19 @@ class IsekaiScanDriver(SupportBase):
             if len(chapters) < 2:
                 continue
             href = chapters[1].get_attribute('href')
+            # do not test cases where chapter numbering is "broken" and cannot
+            # be unified (ex: `chapter-0-2`)
+            sep = self._get_sep(href)
+            if len(href.split('/')[-2].split(sep)[1].split('-')) > 1:
+                to_ignore.append(href)
+            if href in to_ignore:
+                continue
             self._driver.get(href)
+            # pass RPGD cookie/tracking agreement button if any, since only
+            # necessary on the first load during a testing session.
+            cookie_ack_btn = self._driver.find_elements(by=By.CSS_SELECTOR, value='#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.sc-ifAKCX.ljEJIv')
+            if cookie_ack_btn:
+                cookie_ack_btn[0].click()
             return
 
         raise "No manga with enough chapters nor with link on isekaiscan"
@@ -52,3 +75,10 @@ class IsekaiScanDriver(SupportBase):
             return None
         parts = self._driver.find_elements(by=By.CSS_SELECTOR, value="#manga-reading-nav-head .breadcrumb li:nth-child(1) > a")
         return parts[0].text
+
+    def get_chapter(self):
+        sep = self._get_sep(self._driver.current_url)
+        return int(self._driver.current_url.split('/')[-2].split(sep)[1])
+
+    def get_page(self):
+        return None
--- web-extension/support/isekaiscan.js
+++ web-extension/support/isekaiscan.js
@@ -7,10 +7,18 @@ function IsekaiScanComPlugin() {
 
 IsekaiScanComPlugin.prototype.getInfos = function(url, doc) {
     let parts = url.split('/').filter(s => s.length !== 0);
+    let chapter_prefix = null;
+
+    if (url.indexOf('chapter-') !== -1) {
+        chapter_prefix = 'chapter';
+    }
+    else if (url.indexOf('ch-') !== -1) {
+        chapter_prefix = 'ch';
+    }
 
     if (parts.length < 2) {
         return null;
-    } else if (parts[parts.length - 1].indexOf('chapter-') !== 0) {
+    } else if (parts[parts.length - 1].indexOf(`${chapter_prefix}-`) !== 0) {
         // manga page
         let elem = doc.querySelector('.profile-manga>.container .breadcrumb > li:last-child > a');
         if (!elem) {
@@ -24,26 +32,33 @@ IsekaiScanComPlugin.prototype.getInfos = function(url, doc) {
             LOGS.log('S78');
             return null;
         }
-        let id = elem.getAttribute('href').split('/chapter-')[0].split('/manga/')[1];
-        return { common: { name, chapter: null, page: null }, id, homeUrl: url };
+        let id = elem.getAttribute('href').split('/manga/')[1].split('/')[0];
+        return { common: { name, chapter: null, page: null },
+                 id, homeUrl: url, prefixes: {chapter: null}};
     }
     // chapter page
-    let elem = doc.querySelector('#manga-reading-nav-head .breadcrumb > li:nth(1) > a');
+    let elem = doc.querySelector('#manga-reading-nav-head .breadcrumb > li:nth-child(2) > a');
     if (!elem) {
         LOGS.log('S79');
         return null;
     }
     let name = elem.innerText;
     let homeUrl = elem.getAttribute('href');
-    let chapter = parseInt(elem.nextSibling.innerText.split(' ')[1]);
-    let id = homeUrl.split('/chapter-')[0].split('/manga/')[1];
+    // XXX BUG XXX:
+    // Found some chapters numbered `chapter-0-2` (2nd version of chapter 0 ?)
+    // As such, and to provide an uniform reading experience throughout the
+    // supported readers, we'll only keep the first number...
+    // -> We acknowledge this may lead to a non-functional link later on
+    let chapter = parseInt(url.split('/')[3].split('-')[1]);
+    let id = homeUrl.split('/manga/')[1].split('/')[0];
 
-    return { common: { name, chapter, page: null }, id, homeUrl };
+    return { common: { name, chapter, page: null },
+             id, homeUrl, prefixes: {chapter: chapter_prefix}};
 };
 
 IsekaiScanComPlugin.prototype.computeURL = function(comicInfo) {
-    if (comicInfo.common.chapter) {
-        return `https://isekaiscan.com/manga/${comicInfo.id}/chapter-${comicInfo.common.chapter}`;
+    if (comicInfo.common.chapter !== null) {
+        return `https://isekaiscan.com/manga/${comicInfo.id}/${comicInfo.prefixes.chapter}-${comicInfo.common.chapter}`;
     }
     return comicInfo.homeUrl;
 };
