64eae85cbab3811a0b9e5fccfff437d93e47b883
David Pineau <dav.pineau@gmail.com>
2021-08-20 22:33:09 +0200

    [Doc] Add block of comment in background-engine.js which explains the why of this file and some complexity of the project

--- web-extension/background-engine.js
+++ web-extension/background-engine.js
@@ -5,6 +5,41 @@
     LOGS:readable
 */
 
+/*
+ * /!\  IMPORTANT - PLEASE READ  /!\
+ *
+ * There are many reasons in this web-extension for the existence of the
+ * "background" or "extension" page (which uses this script as an entry-point).
+ *
+ * First of all, many contraints are pushed onto the web-extension development
+ * by various security features of the browsers:
+ *  - CORS (Cross Origin Resource Sharing) prevents any piece of code from
+ *  calling specific APIs or loading URLs that are not authorized beforehands
+ *  by the website's developer. This makes the job of a web-extension more
+ *  complex, as many thing are now authorized depending on the context.
+ *  - Permission specification (manifest) & limitations based on the context of
+ *  the script
+ *
+ * This has many implications including:
+ * - Sidebar (frame loading the extension's URL) has no right to inspect the
+ *   parent frame's DOM
+ * - Sidebar (frame loading the extension's URL) cannot interact with browser
+ *   storage
+ *
+ * Also, in a concern to keep the manifest duplication as small as possible for
+ * each of the readers supported, we've chosen to:
+ * - Avoid loading all reader support scripts into all pages
+ *
+ * All these constraints (most of which are enforced by the browser) led us to
+ * increasingly consider the extension page as a central HUB for messaging, and
+ * interacting with the storage implementation, explaining why:
+ *  - All reader support scripts are loaded into the extension page
+ *  - The extension script is mostly about handling messages and computing
+ *    stuff on behalf of other scripts.
+ *  - A lot of apparently simple logics have become a multi-step process of
+ *    message exchanges and computations
+ */
+
 const BACKGROUND_ID = 'BookMyComics/BackgroundScript';
 
 const bmcSources = new BmcSources();
