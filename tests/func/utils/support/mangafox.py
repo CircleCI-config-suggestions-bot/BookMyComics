3a6cf70ba88db303f31f6b8eecbcb2a2e15cbc63
David Pineau <dav.pineau@gmail.com>
2021-09-09 22:46:26 +0200

    [Support/Mangafox] Fix URL generation if on first page and Fix load_random() utility

--- tests/func/utils/support/mangafox.py
+++ tests/func/utils/support/mangafox.py
@@ -121,33 +121,61 @@ class FanFoxDriver(SupportBase):
         # First, go to the website
         self.home()
 
-        # Then randomly choose one of the "latest" chapters from the home page
-        latest_chapters = self._driver.find_elements_by_css_selector(
-            'p.manga-list-1-item-subtitle > a')
-        self._driver.get(
-            latest_chapters[random.randrange(len(latest_chapters))]
-            .get_attribute('href'))
-        if self._driver.current_url == 'https://fanfox.net':
-            raise RetriableError('Could not load a random "latest" chapter')
+        # Select a list of "candidate chapters links" (with the href values,
+        # which prevents invalidating the candidates by loading a new page)
+        mangas = self._driver.find_elements_by_css_selector('.manga-list-4-list > li')
+        print(f'checking out {len(mangas)} mangas')
+        candidates = []
+        while len(mangas):
+            manga = mangas.pop(random.randrange(len(mangas)))
+            chapters = manga.find_elements_by_css_selector('.manga-list-4-item-part > li > a')
+            # Guarantee that we take a chapter which has both a "prev" and a "next"
+            if len(chapters) < 3:
+                print('Skipping, not enough chapters')
+                continue
+            candidates.append(chapters[1].get_attribute('href'))
 
-        # Now, select a page which has both "next" and "prev" pages (ie:
-        # neither first nor last), but first we need to ensure the DOM has been
-        # properly updated, and that the required select is present (it's added
-        # dynamically)
-        try:
-            pages = self._driver.find_elements_by_css_selector('div > div > span > a')
-        except NoSuchElementException:
-            raise RetriableError('No pages buttons found in manga chapter')
-        if len(pages) <= 2:
-            # This might mean that we're looking at one-page chapters for the
-            # manga. As the reader supports it, let's go with this.
-            self._navbar.update()
+        # Check random candidates until they fit the bill:
+        # - Same website (base url)
+        # - Has a navigation bar
+        print(f'checking out {len(candidates)} candidates')
+        while len(candidates):
+            candidate = candidates.pop(random.randrange(len(candidates)))
+            self._driver.get(candidate)
+
+            if self._driver.current_url == 'https://fanfox.net':
+                print('Failed ? Still on homepage')
+                continue
+            print(self._driver.current_url)
+
+            if len(self._driver.current_url.split('/')) != 7:
+                print('Does not support URLS with volume part for now:'
+                      f' {len(self._driver.current_url.split("/"))} parts')
+                continue
+
+            # Now, select a page which has both "next" and "prev" pages (ie:
+            # neither first nor last), but first we need to ensure the DOM has been
+            # properly updated, and that the required select is present (it's added
+            # dynamically)
+            try:
+                pages = self._driver.find_elements_by_css_selector('div > div > span > a')
+            except NoSuchElementException:
+                print('Failed? No navbar')
+                continue
+
+            if len(pages) <= 2:
+                # This might mean that we're looking at one-page chapters for the
+                # manga. As the reader supports it, let's go with this.
+                self._navbar.update()
+            else:
+                # Only retain half the links (the navigation buttons are present twice
+                # in a page, on top of the scan and underneath it), and click on a
+                # page link which is neither the first nor the last.
+                pages = pages[0:int(len(pages)/2)]
+                self._wrapper.ensure_click(pages[random.randrange(1, len(pages) - 2)])
+                self._navbar.update()
             return
-        # Only retain half the links (the navigation buttons are present twice
-        # in a page, on top of the scan and underneath it).
-        pages = pages[0:int(len(pages)/2)]
-        self._wrapper.ensure_click(pages[random.randrange(1, len(pages) - 1)])
-        self._navbar.update()
+        raise RuntimeError("No manga with enough chapters nor with link on mangafox")
 
     def has_prev_page(self):
         return self._navbar.has_prev_page()
