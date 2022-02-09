26014b1e15ae78b1b34a4b152c997eeee42b1c9a
David Pineau <dav.pineau@gmail.com>
2022-02-09 13:58:10 +0100

    [Test/Utils] Remove usage of DesiredCapabilities for driver configuration
    That feature has been deprecated by selenium to the profit of the options objects.

--- tests/func/utils/drivers/chrome.py
+++ tests/func/utils/drivers/chrome.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from .base import BaseWebdriverWrapper
 
@@ -9,19 +8,16 @@ class Wrapper(BaseWebdriverWrapper):
     def __init__(self, *args, **kwargs):
         super(Wrapper, self).__init__(*args, **kwargs)
 
-        capabilities = DesiredCapabilities.CHROME
-        capabilities['loggingPrefs'] = {'browser': 'ALL'}
-
         options = webdriver.ChromeOptions()
         options.add_argument('--load-extension={}'.format(self._ext.unpacked_path))
         options.headless = False
+        options.set_capability('loggingPrefs', {'browser': 'ALL'})
         # Attempt to fix driver.get() getting stuck during tests.
         options.add_argument('--disable-browser-side-navigation')
 
         print('[Chrome] Loading addon from "{}"'.format(self._ext.unpacked_path))
         print('[Chrome] Loading manifest from "{}"'.format(self._ext._manifest_path))
-        self._driver = webdriver.Chrome(options=options,
-                                        desired_capabilities=capabilities)
+        self._driver = webdriver.Chrome(options=options)
 
     def ensure_click(self, element):
         """
